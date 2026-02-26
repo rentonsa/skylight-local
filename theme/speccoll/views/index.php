1007c5e563955410144d510332bb699c6ced1a6d
Scott Renton <rentonsa@gmail.com>
2026-02-26 13:50:28 +0000

    LUC023A-245 Changing front picture

--- theme/speccoll/views/index.php
+++ theme/speccoll/views/index.php
@@ -1,6 +1,7 @@
 <div class="container-fluid">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <img src="https://images.is.ed.ac.uk/luna/servlet/iiif/UoEsha~4~4~63752~102951/750,4941,3383,1789/1700,750/0/default.jpg">
+        <!--<img src="https://images.is.ed.ac.uk/luna/servlet/iiif/UoEsha~4~4~63752~102951/750,4941,3383,1789/1700,750/0/default.jpg">-->
+        <img src="https://images.is.ed.ac.uk/luna/servlet/iiif/UoEwal~1~1~63427~100683/211,379,3021,1511/1000,/0/default.jpg">
     </div>
     <div class="clearfix"></div>
     <div class="spacer"></div>
